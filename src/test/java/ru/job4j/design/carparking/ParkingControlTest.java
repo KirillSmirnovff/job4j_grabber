4578b5eb587de7668f0f1394c69f9b82e68c405d
KirillSmirnovff <smirnovk.1990@gmail.com>
2022-05-20 17:54:41 +0300

    2. Парковка машин [#853 #303187]

---
+++ src/test/java/ru/job4j/design/carparking/ParkingControlTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.design.carparking;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ParkingControlTest {
+
+    @Test
+    public void whenCarSize1AndCellFree() {
+        ParkingPlace carParking = new CarParkingPlace(1);
+        ParkingPlace truckParking = new TruckParkingPlace(1);
+        ParkingControl control = new ParkingControl(carParking, truckParking);
+        Car car = new PassengerCar("Lada Kalina", 1);
+        control.distribute(car);
+        List<Car> expected = List.of(car);
+        assertThat(carParking.getAll(), is(expected));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenCarSize1AndCellNotFree() {
+        ParkingPlace carParking = new CarParkingPlace(1);
+        ParkingPlace truckParking = new TruckParkingPlace(1);
+        ParkingControl control = new ParkingControl(carParking, truckParking);
+        Car carOne = new PassengerCar("Lada Kalina", 0.5);
+        Car carTwo = new PassengerCar("Opel Astra", 1);
+        control.distribute(carOne);
+        control.distribute(carTwo);
+     }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenWrongCarType() {
+        new PassengerCar("Lada Kalina", 2);
+    }
+
+    @Test
+    public void whenCarSize2AndTruckCellFree() {
+        ParkingPlace carParking = new CarParkingPlace(1);
+        ParkingPlace truckParking = new TruckParkingPlace(1);
+        ParkingControl control = new ParkingControl(carParking, truckParking);
+        Car car = new Truck("Zil", 2);
+        control.distribute(car);
+        List<Car> expected = List.of(car);
+        assertThat(truckParking.getAll(), is(expected));
+    }
+
+    @Test
+    public void whenCarSize2AndTruckCellFullAndPassengerCellFree() {
+        ParkingPlace carParking = new CarParkingPlace(3);
+        ParkingPlace truckParking = new TruckParkingPlace(1);
+        ParkingControl control = new ParkingControl(carParking, truckParking);
+        Car carOne = new Truck("Zil", 2);
+        Car carTwo = new Truck("Mercedes", 2);
+        control.distribute(carOne);
+        control.distribute(carTwo);
+        List<Car> expected = List.of(carTwo);
+        assertThat(carParking.getAll(), is(expected));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenCarSize2AndAllCellsFull() {
+        ParkingPlace carParking = new CarParkingPlace(1);
+        ParkingPlace truckParking = new TruckParkingPlace(0);
+        ParkingControl control = new ParkingControl(carParking, truckParking);
+        Car car = new Truck("Zil", 2);
+        control.distribute(car);
+    }
+}
